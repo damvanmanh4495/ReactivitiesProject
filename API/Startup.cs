ed85d69098472e91c72d9e95ab6f1ded3127a863
damvanmanh4495 <damvanmanh441995@gmail.com>
2021-04-02 14:24:32 +0700

    crud activity

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
 
 namespace API
 {
@@ -31,6 +33,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
